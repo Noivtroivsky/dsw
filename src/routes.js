c2d336124afe509d66d6a25479b875a94ae12646
NOÁ SANTANA MIGLIO <noa.smiglio@senacsp.edu.br>
2025-03-18 10:48:19 -0300

    corrigido

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import aluno from './controller/alunoController.js'
 import ator from './controller/atorController.js'
 import turma from './controller/turmaController.js'
+
 export default function adicionarRotas(servidor) {
   servidor.use(aluno);
   servidor.use(ator);
+  servidor.use(turma);
 }
